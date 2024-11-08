6fe2ba0078a06b88fc587d9b89a8ce8989608bb6
Avinash <avinash.gudipudi29@gmail.com>
2024-11-07 19:24:40 -0500

    chanes

--- redactor.py
+++ redactor.py
@@ -288,6 +288,9 @@ def main(args):
     if not os.path.exists(args.output):
         os.mkdir(args.output)
 
+    if args.stats != "stderr" and args.stats != "stdout":
+        log_file = args.stats if args.stats else os.path.join(args.output, "redaction_stats.txt")
+        stats_f = open(log_file, 'w', encoding="utf-8")
     # Process each file, apply redactions, and save the results.
     for i, file_path in enumerate(files_to_process, start=1):
         with open(file_path, "r", encoding="utf-8") as f:
@@ -317,7 +320,7 @@ def main(args):
         
         #print("fyghhhg", detailed_info)
 
-        # Output the statistics and detailed information based on user preference
+        #Output the statistics and detailed information based on user preference
         if args.stats == "stderr":
             sys.stderr.write(f"Printing stats to stderr for file: {file_path}\n")
             format_string = format_entity_stats(os.path.basename(file_path), args, names, dates, phones, addresses,redacted_words_count,redacted_sentences_count)
@@ -331,33 +334,17 @@ def main(args):
             sys.stdout.write("\nDetailed information on identified entities in the file:\n")
             sys.stdout.write(str(detailed_info) + "\n")
         else:
-            log_file = args.stats if args.stats else os.path.join(args.output, "redaction_stats.txt")
+            #
             #print(f"Writing stats to the file: {log_file}")
             logging.info(f"Writing stats to the file: {log_file}")
-            temp_var=count
-            # print(" temp_var",temp_var)
-            # mode= "w" if temp_var == 1 else "a"
-            mode='a'
-            f = open(log_file, mode, encoding="utf-8")
-            try:
-                # temp_var= temp_var - 1
-                # print("track temp_var",temp_var)
-                format_string = format_entity_stats(os.path.basename(file_path), args, names, dates, phones, addresses, redacted_words_count, redacted_sentences_count)
-                f.write(format_string)
-                f.write("\nDetailed information on identified entities in the file:\n")
-                f.write(str(detailed_info) + "\n")
-                f.write("\n")
-            finally:
-                # f.close()
-                temp_var= temp_var - 1
-                if temp_var == 1:
-                    f.close()
-                    #print("File closed")
-                    
-                
-            
+            format_string = format_entity_stats(os.path.basename(file_path), args, names, dates, phones, addresses, redacted_words_count, redacted_sentences_count)
+            stats_f.write(format_string)
+            stats_f.write("\nDetailed information on identified entities in the file:\n")
+            stats_f.write(str(detailed_info) + "\n")
+            stats_f.write("\n")
 
-        
+    if args.stats != "stderr" and args.stats != "stdout":
+        stats_f.close()
 
 # Argument parsing and main function call.
 if __name__ == "__main__":
